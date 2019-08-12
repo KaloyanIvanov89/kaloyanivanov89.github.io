ca7c01ef1e16011374d9c6a41697b629ddacdbba
unknown <kaloyan89@gmail.com>
2019-08-12 17:47:23 +0300

    Updated to Sass

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require("gulp"),
 	cp = require("child_process");
 
 gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
+	return gulp.src( '_assets/scss/**/*.scss' )
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
 		.pipe( gulp.dest( './docs/css/' ) )
@@ -34,20 +34,20 @@ gulp.task("watch", function() {
 		}
 	});
 
-	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+	gulp.watch( '_assets/scss/**/*.scss', gulp.series('css') );
 
 	gulp.watch(
 		[
 			"./*.html",
 			"./_includes/*.html",
 			"./_layouts/*.html",
-			"./_posts/**/*.*"
+			"./_posts/**/*.*",
+			"./scripts/*.js"
 		]
 	).on('change', gulp.series('jekylldev', 'css') );
 
 	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
 	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
-
 });
 
 gulp.task("deploy", gulp.series('jekyllprod', 'css'));
